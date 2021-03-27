94c688e5c8f7c86ce9de910c7332f8c56f4606bf
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-03-26 23:58:03 -0400

    wrote the generatePassword function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,27 +3,67 @@ var generateBtn = document.querySelector("#generate");
 //variables declared and assigned a value
         
 //arrays containing different accepted characters
-var specialChar = ["!", "#", "$", "%", "&", "'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "\\", "\""];
+var specialChar = ["!", "#", "$", "%", "&", "'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
+function generatePassword(){
 
+  //declare variable for password length and character selection
+  //-prompt user for length of password
+  function length(){
+    var passwordLength = prompt("How many characters would you like to include?", "8-129");
+    if ((passwordLength >= 8) && (passwordLength <= 129)){
+      return;
+    }
+    else {
+      alert("Please enter a number between 8 and 129");
+      length();
+    }
+  }
+  
+  var passwordLength = length();
+  
+  function getCharacters(){
+    //prompt user for character types
+    var specialCheck = confirm("Do you want your password to include special characters?");
+    var numericCheck = confirm("Do you want your password to include numeric characters?");
+    var lowercaseCheck = confirm("Do you want your password to include lowercase characters?");
+    var uppercaseCheck = confirm("Do you want your password to include uppercase characters?");
+    
+    //declare variable for combining arrays
+    var charArray = [];
+    //add all desired characters to empty array.
+    if ((specialCheck == false) && (numericCheck == false) && (lowercaseCheck == false) && (uppercaseCheck == false)){
+      alert("Must include at least one character type");
+      getCharacters();
+    }   
+    else { 
+      if (specialCheck){
+        charArray = charArray.concat(specialChar);}
+      else if (numericCheck){
+        charArray = charArray.concat(numericChar);}
+      else if (lowercaseCheck){
+        charArray = charArray.concat(lowercaseChar);}
+      else if (uppercaseCheck){
+        charArray = charArray.concat(uppercaseChar);} 
+    }
+    
+    return charArray; 
+  }
+  
+  var charArray = getCharacters();
+  var randomArray = [];
+  //generate password
+  //loop that runs x times, x being desired length of password.
+  for (i = 0; i < passwordLength.length; i++){
+    randomArray[i] = charArray[Math.floor(Math.random() * charArray.length)]
+  }
 
-            //-generate password
-
-                //-declare variable with a value of an empty array.
-                //-declare variable for generated password.
-                //-conditional statements to ensure all desired characters are included in password and add all desired characters to empty array.
-                //-loop that runs x times, x being desired length of password.
-
-                    //-each time loop runs
-                        //~select a random character from desired character array.
-                        //~add the character to generated password variable.
-
-                //return generated password value.
-
+  var password = randomArray.join('');
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
